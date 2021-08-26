ddc15f8bf7e4533efa585ffcdf8ddf5da05833c3
Maik-Biazi <71233006+Maik-Biazi@users.noreply.github.com>
2021-08-26 14:59:20 -0300

    bd

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,22 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\event;
+
 class EventController extends Controller
 {
     public function  index(){
-        $nome ="Maik";
-    $idade= 21;
-    $arr= [10,20,30,40,50];
+       
+        $events= Event::all();
 
-    $nomes =["Matheus","Maria", "João", "Saulo"];
-
-    return view('welcome',
-    ['nome'=> $nome,
-     'idade2' => $idade,
-     'profissao' => "Programador",
-     'arr' => $arr,
-     'nomes'=> $nomes
-    ]);
+    return view('welcome', ['events'=> $events]);
     }
     public function create(){
         return view('events.create');
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,12 @@
 @extends('layouts.main')
 @section('title','HDC Events')
 
-    @section('content')
-       <h1>Algum titulo</h1>
-       <img src="/img/banner.jpg" alt="banner">
-       @if(10>5)
-         <p>A condição é true</p>
-         @endif
+@section('content')
 
-         <p>{{$nome}}</p>
-         @for($i=0; $i < count($arr); $i++)
-          <p>{{ $arr[$i]}}</p>
-         @endfor
+@foreach($events as $event)
+      <p>{{$event->title}}-- {{$event->description}}</p>
+
+@endforeach
+       
 
 @endsection
